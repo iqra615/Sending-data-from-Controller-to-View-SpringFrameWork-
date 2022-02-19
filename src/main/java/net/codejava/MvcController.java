5469c6806ae005c4458f4d119d4ac2e8b94349a3
iqra615 <67075928+iqra615@users.noreply.github.com>
2022-02-19 18:56:48 +0500

    second commit

--- src/main/java/net/codejava/MvcController.java
+++ src/main/java/net/codejava/MvcController.java
@@ -1,11 +1,13 @@
 package net.codejava;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MvcController {
@@ -37,8 +39,41 @@ public class MvcController {
 		System.out.println("Call from Service Page");
 		return "/service";
 	}
+	
+
 	@RequestMapping("/help")
-	public String help() {
-		return "/help";
+	public ModelAndView help() {
+		
+		//creating model and view object
+		ModelAndView modelAndView = new ModelAndView();
+		
+		//setting the data 
+		modelAndView.addObject("name", "Iqra Shah");
+		modelAndView.addObject("Rollnumber" , 26);
+		LocalDateTime now = LocalDateTime.now();
+		modelAndView.addObject("time", now);
+		modelAndView.addObject("Teacher", "Maryam");
+		
+		
+		//marks
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(12);
+		list.add(66);
+		list.add(29);
+		
+		modelAndView.addObject("marks" , list);
+		
+		
+		//setting the view name
+		modelAndView.setViewName("help");
+		
+		
+		
+		
+		
+		
+		
+		
+		return modelAndView;
 	}
 }
